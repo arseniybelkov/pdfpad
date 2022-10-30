7c1db370d689381def4c116b756d7d4e03327bf6
arseniybelkov <arsfizteh@gmail.com>
2022-10-30 12:08:38 +0300

    add package info

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+import runpy
 
 
 with open("requirements.txt", encoding="utf-8") as file:
     requirements = file.read().splitlines()
 
+with open("README.md", encoding="utf-8") as file:
+    long_description = file.read()
+
+root = Path(__file__).resolve().parent
+folder = root / "pdfpad"
+version = runpy.run_path(folder / "__version__.py")["__version__"]
+
 setup(
     name="pdfpad",
-    version="0.0.1",
+    version=version,
     license="MIT",
     url="https://github.com/arseniybelkov/pdfpad",
     install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
